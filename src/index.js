0b4a49f65b8cf605526f62477304097533fe9648
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-11 17:28:23 +0530

    added reducers in indexjs

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import reportWebVitals from './reportWebVitals';
+// Reducers
+import authReducer from './Controllers/Redux/authSlice'
+import bugReducer from './Controllers/Redux/authSlice'
+import userReducer from './Controllers/Redux/authSlice'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Redux configure
+const reducer=combineReducers({
+  auth:authReducer,
+  bug:bugReducer,
+  user:userReducer,
+})
+const store=configureStore({
+  reducer
+})
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+ReactDOM.render(
+  <Provider store={(store)}>
+  <App/>
+  </Provider>
+)
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
