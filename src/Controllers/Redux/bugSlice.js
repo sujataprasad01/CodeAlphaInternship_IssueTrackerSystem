05628b3c8c1f4bab60941b2acd00fdbf23c0d5fa
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-11 17:03:24 +0530

    done with redux, bugController, models

---
+++ src/Controllers/Redux/bugSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const slice=createSlice({
+    name:'bug',
+    initialState:[],
+    reducers:{
+        getBugs:(state)=>{
+
+        },
+        createBugs:(state, actions)=>{
+
+        },
+        updateBug:(state, actions)=>{
+
+        },
+        markcomplete:(state, actions)=>{
+            
+        }
+    }
+})
---
+++ src/Controllers/bugController.js
@@ -0,0 +1,9 @@
+import bugModel from '../Models/bugModel'
+
+export function retrieveBugs(){
+    let data=[];
+
+    data.push(new bugModel({
+        _id:23
+    }))
+}
